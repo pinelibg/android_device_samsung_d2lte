9b06817e118c7f5bb5d49653efa1c7c46ebf7df7
HomuHomu <ecoco666@gmail.com>
2014-02-26 01:16:34 +0900

    d2dcm: fixed PRODUCT_COPY_FILES path
    Change-Id: I7213e2a4c28b82065482134dbe8fd61c1782f006

--- full_d2lte.mk
+++ full_d2lte.mk
@@ -27,8 +27,8 @@
 
 # d2dcm ramdisk
 PRODUCT_COPY_FILES += \
-    device/samsung/d2lte/rootdir/sbin/felica_init.sh:root/sbin/felica_init.sh \
-    device/samsung/d2lte/rootdir/init.carrier.rc:root/init.carrier.rc
+    device/samsung/d2lte/rootdir/etc/sbin/felica_init.sh:root/sbin/felica_init.sh \
+    device/samsung/d2lte/rootdir/etc/init.carrier.rc:root/init.carrier.rc
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
